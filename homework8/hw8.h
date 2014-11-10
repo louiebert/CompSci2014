f0e30429fdf858133f8884284b814c8261ccc978
Louie Bertoncin <jbbetoncin0620@lsr7.net>
2014-11-09 23:53:31 -0600

    Added comments

--- homework8/hw8.h
+++ homework8/hw8.h
@@ -48,8 +48,7 @@ const int SURGERY_ARR_LENGTH = 20;
 void greeting();
 
 // open_connection() opens and tests a connection to a data file.
-// Pre: Data file must have same name as the value stored in the var holding
-//  the data file name.
+// Pre: Data file must have same name as the filename variable.
 // Post: Connects to specified data file or exits program if unable to connect.
 void open_connection(ifstream & the_stream, const string & filename);
 
@@ -61,21 +60,22 @@ void input_complaint(string & input);
 // make_array() takes a long source char array and creates a two dimensional
 //   array with the words separated by spaces. Returns # of words in target.
 // Pre: source[] must not have a word exceeding 21 characters. source[] also
-//   shouldn't ever have two spaces in a row. target[] must have the second
-//   dimension as 22 (second square bracket number).
+//   shouldn't ever have two spaces in a row.
 // Post: Makes a 2D array from the words of source. Punctuation is omitted in
 //   the final target array. Returns number of indexes (words) in target.
 int make_array(const char source[], char target[][WORD_LENGTH]);
 
 // disp_words() displays a message to the screen composed of half of the words
 //   (but not more than 5 words) from the user's complaint.
-// Pre:
+// Pre: Second dimension of arr[] needs to be ntca's. num_words must be
+//   positive.
 // Post: Displays a message to the screen composed of half of the words
 //   (but not more than 5 words) chosen at random from user's complaint.
 void disp_words(const char arr[][WORD_LENGTH], const int & num_words);
 
 // keyword() searches the user's input for keywords and returns the keyword.
-// Pre: Array submitted as a parameter must be null-terminated.
+// Pre: Second dimension of arr[] needs to be ntca's. num_words must be
+//   positive.
 // Post: Returns name of file corresponding to name of body part found.
 string keyword(char arr[][WORD_LENGTH], const int & num_words);
 
@@ -88,7 +88,7 @@ string keyword(char arr[][WORD_LENGTH], const int & num_words);
 bool disp_prog(const string & filename);
 
 // random_choice() chooses random number between a given minimum and maximum.
-// Pre: rand() must be seeded.
+// Pre: rand() must be seeded. max must be greater than min.
 // Post: Returns a random number between a given minimum and maximum.
 int random_choice(const int min, const int max);
 
@@ -99,15 +99,12 @@ int num_el(ifstream & fin);
 
 // disp_drug() randomly generates a name of a drug and displays that name to
 //  the screen along with the number of pills to be taken.
-// Pre: The var holding the filename of should match the actual filename.
-//   The filename should be in the same directory as this program.
-// Post: Displays a name of a drug to the screen.
+// Pre: num should be positive.
+// Post: Displays a name of a drug to the screen with the number of pills.
 void disp_drug(const int & num);
 
 // disp_surg() randomly generates a surgery for the patient to undergo
-// Pre: The var holding the name of the file continaing the surgery types
-//   matches the actual filename. The filename should be in same directory as
-//   this program.
+// Pre: None.
 // Post: Displays name of surgery to screen.
 void disp_surg();
 
@@ -118,7 +115,7 @@ void disp_surg();
 bool do_again();
 
 // The goodbye() function prints a farewell to the console.
-// Pre: None
+// Pre: None.
 // Post: Prints a message to the console
 void goodbye();
 
