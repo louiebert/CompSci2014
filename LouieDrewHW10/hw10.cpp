05bbcd2d3ffdb45807211072980587f9d4b8d581
Louie Bertoncin <jbbetoncin0620@lsr7.net>
2014-12-02 14:47:38 -0600

    Setting up hw 10

--- LouieDrewHW10/hw10.cpp
+++ LouieDrewHW10/hw10.cpp
@@ -1,1 +1,35 @@
-//Header
+//Programmer: LOUIE BERTONCIN, DREW BUELTER    Date: 11/30/2014
+//File: hw10.cpp                               Class: CS 1570, Section D
+//Purpose: Main file for hospital tour program
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "patient.h"
+#include "x_rayer.h"
+#include "hospital_room.h"
+
+int main()
+{
+  srand(time(NULL));
+  cout<<"------------Hospital Simulation------------"<<endl<<endl;
+  patient pat1;
+  patient pat2;
+  patient pat3;
+  hospital_room room;
+  cout<<endl<<"Before being admited, "<<pat1<<endl<<endl;
+  cout<<pat2<<endl<<endl;
+  cout<<pat3<<endl<<endl;
+  cout<<"Room Before Use: "<<room<<endl<<endl;
+  room.admit(pat1);
+  room.admit(pat1);
+  room.admit(pat1);
+  room.admit(pat1);
+  room.admit(pat1);
+  cout<<"After being admited, "<<pat1<<endl<<endl;
+  cout<<pat2<<endl<<endl;
+  cout<<pat3<<endl<<endl;
+  cout<<"Room After Use: "<<room<<endl<<endl;
+  cout<<"------------Conclusion of Simulation-------------"<<endl;
+  return 0;
+}
---
+++ LouieDrewHW10/patient.cpp
@@ -0,0 +1,69 @@
+//Programmer: LOUIE BERTONCIN, DREW BUELTER     Date: 11/30/2014
+//File: patient.cpp                             Class: CS 1570, Section D
+//Purpose: Body file for patient class
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include "patient.h"
+using namespace std;
+
+
+patient::patient()
+{
+  short count=0;
+  short numNames=0;
+  short randNum;
+  ifstream in;
+  m_condition=rand()%(MAX_CONDITION-MIN_CONDITION+1)+MIN_CONDITION;
+  m_money=rand()%(MAX_MONEY-MIN_MONEY+1)+MIN_MONEY;
+  if(m_condition==0)
+    m_is_alive=false;
+  else
+    m_is_alive=true;
+  in.open(name_file);
+  while(in>>m_name)
+  {
+    numNames++;
+  }
+  in.close();
+  randNum=rand()%numNames;
+  in.open(name_file);
+  in>>m_name;
+  while(count!=randNum)
+  {
+    in>>m_name;
+    count++;
+  }
+  count=0;
+  numNames=0;
+  in.close();
+}
+
+void patient::pay_out(const float pay_amount)
+{
+  m_money-=pay_amount;
+  return;
+}
+
+void patient::kill()
+{
+  if(m_condition==0)
+    m_is_alive=false;
+  return;
+}
+
+void patient::modify_health(const int condition_dif)
+{
+  m_condition+=condition_dif;
+  kill();
+  return;
+}
+
+ostream& operator <<(ostream& o,const patient& p)
+{
+  o<<p.m_name<<" has $"<<p.m_money<<", a health value of "<<p.m_condition
+  <<", and is "<<(p.m_is_alive?"":"not ")<<"alive.";
+  return o;
+}
