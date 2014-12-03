f58a4b2e72bc44427c03d06522b9f0bf6d07ffa5
Louie Bertoncin <jbbetoncin0620@lsr7.net>
2014-12-02 18:44:15 -0600

    Should be done with Doctor class

--- LouieDrewHW10/doctor.cpp
+++ LouieDrewHW10/doctor.cpp
@@ -1,5 +1,5 @@
-//Programmer: LOUIE BERTONCIN     Date: 11/30/2014
-//File: doctor.cpp                Class: CS 1570, Section D
+//Programmer: LOUIE BERTONCIN, DREW BUELTER     Date: 11/30/2014
+//File: doctor.cpp                              Class: CS 1570, Section D
 //Purpose: Body file for doctor class
 
 #include "doctor.h"
@@ -8,22 +8,22 @@ using namespace std;
 
 Doctor::Doctor(const string& theName)
 {
-  money = 0;
-  name = theName;
+  m_money = 0;
+  m_name = theName;
 }
 
 void Doctor::puke()
 {
-  oz_schraut = 0;
+  m_oz_schraut = 0;
 
   return;
 }
 
 void Doctor::drink_schraut(const short& amount)
 {
-  oz_schraut += amount;
+  m_oz_schraut += amount;
 
-  if(oz_schraut >= MAX_OZ_SCHRAUT)
+  if(m_oz_schraut >= MAX_OZ_SCHRAUT)
   {
     puke();
   }
@@ -31,14 +31,9 @@ void Doctor::drink_schraut(const short& amount)
   return;
 }
 
-short Doctor::get_oz_schraut() const
-{
-  return oz_schraut;
-}
-
 void Doctor::increase_money(const float& amount)
 {
-  money += amount;
+  m_money += amount;
 
   return;
 }
