5b272126ac18c10415048910abb3d4032f6162ac
Louie Bertoncin <jbbetoncin0620@lsr7.net>
2014-12-08 14:40:22 -0600

    clearing up some stuff before I switch branches

--- louie/hw/hw10/liposuctionizer.cpp
+++
@@ -1,3 +0,0 @@
-//Programmer: LOUIE BERTONCIN     Date: 11/30/2014
-//File: liposuctionizer.cpp       Class: CS 1570, Section D
-//Purpose: Body file for liposuctionizer class
--- louie/hw/hw10/organ_donor.h
+++
@@ -1,3 +0,0 @@
-//Programmer: LOUIE BERTONCIN     Date: 11/30/2014
-//File: organ_donor.h             Class: CS 1570, Section D
-//Purpose: Header file for organ_donor class
