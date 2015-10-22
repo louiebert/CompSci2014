7f4a091a94184d2188f604b9b61485161e21c01f
louman347 <louiebertoncin@gmail.com>
2015-10-21 21:17:54 -0500

    The set string length works

--- Algorithms_AlexLouie/resources/generator.cpp
+++ Algorithms_AlexLouie/resources/generator.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
-#include <cstdlib>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -15,7 +16,7 @@ int main() {
   const int NUMSTRINGS = 100;
   char mode;
   int range1, range2;
-  string output;
+  string output = "";
 
   while (!isFinished) {
     cout << "Welcome to random string generator!\n\n" << endl;
@@ -28,12 +29,13 @@ int main() {
     if (isalpha(mode))
       mode = tolower(mode);
 
-    if (mode != 's' || mode != 'r' || mode != 'd') {
+    if (!(mode == 's' || mode == 'r' || mode == 'd')) {
       bool incorrect = true;
       do {
         cout << "That was a bad input! Please try again..." << endl;
         cout << "\tSet Length - s" << endl;
         cout << "\tor Random Length Within a Range - r\n" << endl;
+        cout << "\tor you're Done with this program - d\n" << endl;
         cout << "Your selection:";
         cin >> mode;
         if (isalpha(mode))
@@ -50,17 +52,17 @@ int main() {
       cin >> range1;
       range1 = abs(range1);
 
-      filename = getFileName();
+      string filename = getFileName();
       ofstream outputFile;
       outputFile.open(filename);
       for (int i = 0; i < NUMSTRINGS; ++i) {
         output = "";
         for (int j = 0; j < range1; ++j) {
-          char ltr = 'a' + rand()%26;
+          char ltr = 'a' + rand()%26; // Makes a random character
           if (rand()%2) {
             ltr = toupper(ltr);
           }
-          output.append(ltr);
+          output += ltr ;
         }
         outputFile << output << endl;
       }
@@ -79,11 +81,8 @@ int main() {
 }
 
 string getFileName() {
-  time_t curTime = time(0);
-  stringstream ss;
-  ss << curTime;
   string filename = "greedy_";
-  filename.append(ss.str());
+  filename.append(to_string(time(0)));
   filename.append(".txt");
   return filename;
 }
