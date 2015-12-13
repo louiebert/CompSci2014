3e9d8b06578b8a85b9c43dccb5a62683436b58fc
louman347 <louiebertoncin@gmail.com>
2015-12-13 10:39:24 -0600

    Unweighted shortest path distribution is done. Also, text files are now being ignored.

--- AlgorithmsLouie_Proj2/social.cpp
+++ AlgorithmsLouie_Proj2/social.cpp
@@ -37,17 +37,24 @@ int unweightOutDeg(DVertex v);
 int weightedInDeg(DVertex v, DGraph &dgraph);
 int weightedOutDeg(DVertex v);
 
-void directedDegreeDistribution(DGraph &dgraph, int maxWeight);
+void directedDegreeDistribution(DGraph &dgraph, int maxWeight, ofstream &out);
+
+void floydWarshallWins(UnGraph &ungraph, DGraph &dgraph, vector<Edge> &edges, ofstream &out);
 
 
 int main() {
+  ofstream out;
+  out.open("output.txt");
+
   UnGraph undirectedGraph;
   DGraph directedGraph;
   vector<Edge> edgeList;
   int maxWeight = makeGraphFromFile(GRAPHFILENAME, edgeList, undirectedGraph, directedGraph);
 
-  directedDegreeDistribution(directedGraph, maxWeight);
+  // directedDegreeDistribution(directedGraph, maxWeight, out);
+  floydWarshallWins(undirectedGraph, directedGraph, edgeList, out);
 
+  out.close();
   return 0;
 }
 
@@ -102,7 +109,10 @@ int makeGraphFromFile(string filename, vector<Edge> &list, UnGraph &unGraph, DGr
   return maxWeight;
 }
 
-void directedDegreeDistribution(DGraph &dgraph, int maxWeight) {
+/*
+/   Directed Degree Distribution
+*/
+void directedDegreeDistribution(DGraph &dgraph, int maxWeight, ofstream &out) {
   vector<int> unweightedInDegree (maxWeight + 1, 0);
   vector<int> unweightedOutDegree (maxWeight + 1, 0);
   vector<int> weightedInDegree (maxWeight + 1, 0);
@@ -173,56 +183,56 @@ void directedDegreeDistribution(DGraph &dgraph, int maxWeight) {
   }
 
 
-  cout << "\nUnweighted In Degree:" << endl;
-  cout << "\tMax: " << maxUnInDegree << endl;
-  cout << "\tMax Vertices:" << endl;
+  out << "\nUnweighted In Degree:" << endl;
+  out << "\tMax: " << maxUnInDegree << endl;
+  out << "\tMax Vertices:" << endl;
   for (int i = 0; i < maxUnInDegrees.size(); ++i) {
-    cout << "\t\t" << maxUnInDegrees[i].name << endl;
+    out << "\t\t" << maxUnInDegrees[i].name << endl;
   }
-  cout << "\tDegree Distributions:" << endl;
+  out << "\tDegree Distributions:" << endl;
   for (int i = 0; i < unweightedInDegree.size(); ++i) {
     if (unweightedInDegree[i] > 0)
-      cout << "\t\t" << i << ": " << unweightedInDegree[i] << endl;
+      out << "\t\t" << i << ": " << unweightedInDegree[i] << endl;
   }
 
-  cout << "\nUnweighted Out Degree:" << endl;
-  cout << "\tMax: " << maxUnOutDegree << endl;
-  cout << "\tMax Vertices:" << endl;
+  out << "\nUnweighted Out Degree:" << endl;
+  out << "\tMax: " << maxUnOutDegree << endl;
+  out << "\tMax Vertices:" << endl;
   for (int i = 0; i < maxUnOutDegrees.size(); ++i) {
-    cout << "\t\t" << maxUnOutDegrees[i].name << endl;
+    out << "\t\t" << maxUnOutDegrees[i].name << endl;
   }
-  cout << "\tDegree Distributions:" << endl;
+  out << "\tDegree Distributions:" << endl;
   for (int i = 0; i < unweightedOutDegree.size(); ++i) {
     if (unweightedOutDegree[i] > 0)
-      cout << "\t\t" << i << ": " << unweightedOutDegree[i] << endl;
+      out << "\t\t" << i << ": " << unweightedOutDegree[i] << endl;
   }
 
-  cout << "\nWeighted In Degree:" << endl;
-  cout << "\tMax: " << maxWeightInDegree << endl;
-  cout << "\tMax Vertices:" << endl;
+  out << "\nWeighted In Degree:" << endl;
+  out << "\tMax: " << maxWeightInDegree << endl;
+  out << "\tMax Vertices:" << endl;
   for (int i = 0; i < maxWeightInDegrees.size(); ++i) {
-    cout << "\t\t" << maxWeightInDegrees[i].name << endl;
+    out << "\t\t" << maxWeightInDegrees[i].name << endl;
   }
-  cout << "\tDegree Distributions:" << endl;
+  out << "\tDegree Distributions:" << endl;
   for (int i = 0; i < weightedInDegree.size(); ++i) {
     if (weightedInDegree[i] > 0)
-      cout << "\t\t" << i << ": " << weightedInDegree[i] << endl;
+      out << "\t\t" << i << ": " << weightedInDegree[i] << endl;
   }
 
-  cout << "\nWeighted Out Degree:" << endl;
-  cout << "\tMax: " << maxWeightOutDegree << endl;
-  cout << "\tMax Vertices:" << endl;
+  out << "\nWeighted Out Degree:" << endl;
+  out << "\tMax: " << maxWeightOutDegree << endl;
+  out << "\tMax Vertices:" << endl;
   for (int i = 0; i < maxWeightOutDegrees.size(); ++i) {
-    cout << "\t\t" << maxWeightOutDegrees[i].name << endl;
+    out << "\t\t" << maxWeightOutDegrees[i].name << endl;
   }
-  cout << "\tDegree Distributions:" << endl;
+  out << "\tDegree Distributions:" << endl;
   for (int i = 0; i < weightedOutDegree.size(); ++i) {
     if (weightedOutDegree[i] > 0)
-      cout << "\t\t" << i << ": " << weightedOutDegree[i] << endl;
+      out << "\t\t" << i << ": " << weightedOutDegree[i] << endl;
   }
 
+  return;
 }
-
 int unweightInDeg(DVertex v, DGraph &dgraph) {
   int total = 0;
   for (int i = 0; i < dgraph.vertices.size(); ++i){
@@ -235,11 +245,9 @@ int unweightInDeg(DVertex v, DGraph &dgraph) {
   }
   return total;
 }
-
 int unweightOutDeg(DVertex v) {
   return v.outgoingEdges.size();
 }
-
 int weightedInDeg(DVertex v, DGraph &dgraph) {
   int total = 0;
   for (int i = 0; i < dgraph.vertices.size(); ++i){
@@ -252,11 +260,63 @@ int weightedInDeg(DVertex v, DGraph &dgraph) {
   }
   return total;
 }
-
 int weightedOutDeg(DVertex v) {
   int total = 0;
   for (int i = 0; i < v.outgoingEdges.size(); ++i)
     total += v.outgoingEdges[i].weight;
   return total;
+}
+
+
+/*
+/   Everything else
+*/
+void floydWarshallWins(UnGraph &ungraph, DGraph &dgraph, vector<Edge> &edges, ofstream &out) {
+  vector< vector<int> > distUn (ungraph.vertices.size(), vector<int> (ungraph.vertices.size(), 1000000));
+  vector<Edge> edgesUn (edges);
+  for (int i = 0; i < ungraph.vertices.size(); ++i)
+    distUn[i][i] = 0;
+  for (int i = 0; i < edgesUn.size(); ++i) {
+    int target, source;
+    for (int j = 0; j < ungraph.vertices.size(); ++j) {
+      if (ungraph.vertices[j].name == edgesUn[i].source) {
+        source = j;
+        break;
+      }
+    }
+    for (int j = 0; j < ungraph.vertices.size(); ++j) {
+      if (ungraph.vertices[j].name == edgesUn[i].target) {
+        target = j;
+        break;
+      }
+    }
+    distUn[target][source] = 1;
+    distUn[source][target] = 1;
+  }
+  for (int k = 0; k < ungraph.vertices.size(); ++k) {
+    for (int i = 0; i < ungraph.vertices.size(); ++i) {
+      for (int j = 0; j < ungraph.vertices.size(); ++j) {
+        if (distUn[i][j] > distUn[i][k] + distUn[k][j])
+          distUn[i][j] = distUn[i][k] + distUn[k][j];
+      }
+    }
+  }
+
+  /*  UNWEIGHTED SHORTEST PATH DISTRIBUTION
+  vector<int> maxUns;
+  for (int i = 0; i < distUn.size(); ++i) {
+    for (int j = i + 1; j < distUn[i].size(); ++j) {
+      if (distUn[i][j] >= maxUns.size())
+        maxUns.resize(distUn[i][j] + 1);
+      maxUns[distUn[i][j]] += 1;
+    }
+  }
+  out << "\n\nUnweighted Shortest Path Distribution:" << endl;
+  for (int i = 0; i < maxUns.size(); ++i) {
+    if (maxUns[i] > 0)
+      out << "\t" << i << ": " << maxUns[i] << endl;
+  }*/
+
+  return;
 }
 
